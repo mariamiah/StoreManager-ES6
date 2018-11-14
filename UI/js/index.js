2ef8d88e59cb2fb5ad3afabc4c9b54a62294b36f
= <=>
2018-11-14 21:16:15 +0300

    Modify Login page

--- UI/js/index.js
+++ UI/js/index.js
@@ -7,18 +7,33 @@ document.getElementById('loginuser').addEventListener
 function login(e){
     e.preventDefault();
     let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
+    let password = document.getElementById('current-password').value;
     fetch(request, {
         method: 'POST',
         headers: {
             'Accept': 'application/json, text/plain, */*',
-            'Content-type': 'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({username:username, password:password})
     })
-    .then((res)=> res.json())
-    .then((data)=> console.log(data))
-    .catch(function(error){
-        console.log(error);
+    .then((res)=> {
+        return res.json();
+    })
+    .then(function(json){
+        if ('token' in json){
+            localStorage.setItem('token', json['token'])
+            localStorage.setItem('username', username)
+            let base64token = json['token'].split('.')[1];
+            let realtoken = base64token.replace(/-/g, '+').replace(/_/g, '/');
+            let decoded_token = JSON.parse(window.atob(realtoken));
+            if(decoded_token['roles']=="Admin"){
+                window.location.assign('UI/admin_dashboard.html')
+            }else{
+                window.location.assign('UI/dashboard.html')
+            }
+        }else{
+            console.log(json)
+            document.getElementById('message').innerHTML;
+        }
     })
 }
