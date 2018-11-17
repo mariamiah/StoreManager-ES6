2885a0f3c274b6fe952a7c0ecb29c51f54a82ad1
= <=>
2018-11-17 17:26:37 +0300

    Add categories.js[Delivers #162031506]

---
+++ UI/js/category.js
@@ -0,0 +1,28 @@
+// Add Event listener
+document.getElementById('addCategoryForm').addEventListener('submit', addCategory);
+
+function addCategory(e){
+    e.preventDefault()
+    //Fetch API to add a new category
+    const categoryAPI = "https://storemanager15.herokuapp.com/api/v2/categories"
+    let categoryName = document.getElementById('categoryName').value;
+    fetch(categoryAPI, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json, */*',
+            'Authorization': 'Bearer '+ localStorage.getItem('token')
+        },
+        body: JSON.stringify({category_name: categoryName})
+    })
+    .then((response)=> {
+        return response.json()
+    })
+    .then((json)=> {
+        if (json){
+            console.log(json)
+            document.getElementById('newCategoryMessage').innerHTML = json.message
+        }
+    })
+}
+
